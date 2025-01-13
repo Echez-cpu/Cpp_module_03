1a9554d5012ca815d896eddda11b52944a8f21e8
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-01-13 19:22:17 +0100

    Update ScavTrap.cpp

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -7,3 +7,32 @@ ScavTrap::ScavTrap(str name) : ClapTrap() {
 	this->setAttackDamage(20);
 	std::cout << "A very deadly ScavTrap has appeared, be careful with " << this->name << " and his " << this->attackDamage << " attack damage !" << std::endl;
 }
+
+
+ScavTrap::ScavTrap(ScavTrap const &source) : ClapTrap(source.getName()) {
+	*this = source;
+}
+
+ScavTrap::~ScavTrap() {
+	std::cout << "Deadly ScavTrap " << this->name << " exploded." << std::endl;
+}
+
+ScavTrap &	ScavTrap::operator=(ScavTrap const &original_copy) {
+	if (this != &original_copy) {
+		this->name = original_copy.getName();
+		this->hitPoints = original_copy.getHitPoints();
+		this->energyPoints = original_copy.getEnergyPoints();
+		this->attackDamage = original_copy.getAttackDamage();
+	}
+	return *this;
+}
+
+
+
+void	ScavTrap::attack(const str &target) {
+	std::cout << "Deadly ScavTrap " << this->name << " go for broke " << target << " causing him " << this->attackDamage << " attack damage" << std::endl;
+}
+
+void	ScavTrap::guardGate() const {
+	std::cout << "Violence ScavTrap " << this->name << " enter in protected mode !" << std::endl;
+}
