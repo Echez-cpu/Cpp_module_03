e67964c7990b4f47c492a87c35332a838d2ffe57
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-01-13 16:47:27 +0100

    Update ClapTrap.hpp

--- ex01/ClapTrap.hpp
+++ ex01/ClapTrap.hpp
@@ -11,6 +11,7 @@ class ClapTrap {
 	public:
 		ClapTrap(str name);
 		ClapTrap(ClapTrap const &source);
+		ClapTrap();		
 		~ClapTrap();
 
 		ClapTrap &	operator=(ClapTrap const &rhSide);
@@ -18,9 +19,11 @@ class ClapTrap {
 		int		getHitPoints() const;
 		int		getEnergyPoints() const;
 		int		getAttackDamage() const;
+		str		getName() const;
 		void	setHitPoints(int hitPoints);
 		void	setEnergyPoints(int energyPoints);
 		void	setAttackDamage(int attackDamage);
+		void	setName(str name);
 
 		void	attack(const str &target);
 		void	takeDamage(unsigned int amount);
