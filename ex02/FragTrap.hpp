0021c315a5cb0fb356e2638300269394cca7c17d
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-01-14 21:21:16 +0100

    Update FragTrap.hpp

--- ex02/FragTrap.hpp
+++ ex02/FragTrap.hpp
@@ -1,1 +1,20 @@
 
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap {
+	public:
+		FragTrap(str name);
+		FragTrap(FragTrap const &source);
+		~FragTrap();
+
+		FragTrap& operator=(FragTrap const &original_copy);
+		
+	void	highFivesGuys(void);
+
+  private:
+};
+
+#endif
