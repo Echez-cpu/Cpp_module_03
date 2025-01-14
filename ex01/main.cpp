3c6c900bd90be87efe4b56ae39d106ff56e0c445
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-01-14 17:24:58 +0100

    Update main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,48 +1,54 @@
 
 #include "ScavTrap.hpp"
 
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
 int main() {
-    // Create a ScavTrap object
-    std::cout << "=== Creating ScavTrap objects ===" << std::endl;
-    ScavTrap scav1("Scavvy");
-    ScavTrap scav2("Trapster");
+    std::cout << "\n=== Creating ScavTrap instances ===" << std::endl;
+    ScavTrap scav1("GateKeeper");
+    ScavTrap scav2("Guardian");
 
-    // Test attack functionality
     std::cout << "\n=== Testing attack ===" << std::endl;
-    scav1.attack("an intruder");
-    scav2.attack("a target");
+    scav1.attack("Intruder");
+    scav2.attack("Enemy");
 
-    // Test taking damage
     std::cout << "\n=== Testing takeDamage ===" << std::endl;
     scav1.takeDamage(30);
-    scav2.takeDamage(20);
+    scav2.takeDamage(70);
 
-    // Test repairing
     std::cout << "\n=== Testing beRepaired ===" << std::endl;
     scav1.beRepaired(20);
-    scav2.beRepaired(15);
+    scav2.beRepaired(50);
 
-    // Test energy depletion
     std::cout << "\n=== Testing energy depletion ===" << std::endl;
-    for (int i = 0; i < 60; ++i) { // Loop to drain energy
-        scav1.attack("dummy target");
+    for (int i = 0; i < 55; ++i) {  // Loop to deplete energy
+        scav1.attack("Dummy Target");
     }
+    scav1.attack("Exhausted Target");
 
-    // Test guardGate functionality
     std::cout << "\n=== Testing guardGate ===" << std::endl;
+    scav1.guardGate();
     scav2.guardGate();
 
-    // Test copy constructor
-    std::cout << "\n=== Testing copy constructor ===" << std::endl;
-    ScavTrap scav3(scav1);
-    scav3.attack("a clone target");
-
-    // Test copy assignment operator
-    std::cout << "\n=== Testing copy assignment operator ===" << std::endl;
-    scav3 = scav2;
-    scav3.attack("another target");
-
-    // Destructor messages will display automatically at the end of the program
-    std::cout << "\n=== End of program ===" << std::endl;
+    std::cout << "\n=== Testing destruction order ===" << std::endl;
+    // Objects will be destroyed in reverse order of creation automatically.
     return 0;
+}
+
+
+
+int	main() {
+	ClapTrap	Angel("Angel");
+	ClapTrap	*Human = new ClapTrap("Human");
+	ScavTrap	Demon("Demon");
+
+	Angel.attack("Demon");
+	Demon.takeDamage(Robin.getAttackDamage());
+	Demon.beRepaired(18);
+    Demon.guardGate();
+	Demon.attack("Human");
+	Human->takeDamage(Demon.getAttackDamage());
+	delete Human;
+	return 0;
 }
