62659fa02400e19edf636478826c176276793a82
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-01-14 21:04:34 +0100

    Update ScavTrap.hpp

--- ex02/ScavTrap.hpp
+++ ex02/ScavTrap.hpp
@@ -1,1 +1,20 @@
 
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+	public:
+		ScavTrap(str name);
+		ScavTrap(ScavTrap const &source);
+		~ScavTrap();
+
+		ScavTrap& operator=(ScavTrap const &original_copy);
+		
+		void	attack(const str &target);
+		void	guardGate() const;
+	private:
+};
+
+#endif
