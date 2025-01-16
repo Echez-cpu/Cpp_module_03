089270a2a87aa401af7520257afc2de6ba9d90b5
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-01-16 20:07:07 +0100

    Create ScavTrap.hpp

---
+++ ex03/ScavTrap.hpp
@@ -0,0 +1,19 @@
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : virtual public ClapTrap {
+	public:
+		ScavTrap(str name);
+		ScavTrap(ScavTrap const &source);
+		~ScavTrap();
+
+		ScavTrap& operator=(ScavTrap const &original_copy);
+		
+		void	attack(const str &target);
+		void	guardGate() const;
+	private:
+};
+
+#endif
