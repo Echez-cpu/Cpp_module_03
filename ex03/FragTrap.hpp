4d06bf8e0157926a921a69df77965086e856cd7e
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-01-16 20:43:09 +0100

    Update FragTrap.hpp

--- ex03/FragTrap.hpp
+++ ex03/FragTrap.hpp
@@ -7,6 +7,7 @@ class FragTrap : virtual public ClapTrap {
 	public:
 		FragTrap(str name);
 		FragTrap(FragTrap const &source);
+		FragTrap();
 		~FragTrap();
 
 		FragTrap& operator=(FragTrap const &original_copy);
