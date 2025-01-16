ba9145cf758347bf6a68a70da6c1ddef09289b34
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-01-16 20:05:07 +0100

    Create FragTrap.cpp

---
+++ ex03/FragTrap.cpp
@@ -0,0 +1,36 @@
+#include "FragTrap.hpp"
+
+
+FragTrap::FragTrap(str name) : ClapTrap() {
+	this->name = name;
+	this->hitPoints = 100;
+	this->energyPoints = 100;
+	this->attackDamage = 30;
+  std::cout << "FragTrap " << this->name << " is ready for combat! (Constructor called)\n";
+}
+
+
+FragTrap::FragTrap(FragTrap const &source) {
+	*this = source;
+}
+
+
+FragTrap::~FragTrap() {
+  std::cout << "FragTrap " << this->name << " has been destroyed! (Destructor called)\n";
+}
+
+
+FragTrap&	FragTrap::operator=(FragTrap const &original_copy) {
+	if (this != &original_copy) {
+		this->name = original_copy.getName();
+		this->hitPoints = original_copy.getHitPoints();
+		this->energyPoints = original_copy.getEnergyPoints();
+		this->attackDamage = original_copy.getAttackDamage();
+	}
+	return *this;
+}
+
+
+void	FragTrap::highFivesGuys() {
+	 std::cout << "FragTrap " << name << " requests a high five! ✋\n";
+}
