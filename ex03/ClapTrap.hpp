65ecb3d146dc52b3bb3a5bf08e067899f99fd56d
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-01-16 19:58:30 +0100

    Create ClapTrap.hpp

---
+++ ex03/ClapTrap.hpp
@@ -0,0 +1,39 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+typedef std::string str;
+
+class ClapTrap {
+	public:
+		ClapTrap(str name);
+		ClapTrap(ClapTrap const &source);
+		ClapTrap();		
+		~ClapTrap();
+
+		ClapTrap &	operator=(ClapTrap const &rhSide);
+
+		int		getHitPoints() const;
+		int		getEnergyPoints() const;
+		int		getAttackDamage() const;
+		str		getName() const;
+		void	setHitPoints(int hitPoints);
+		void	setEnergyPoints(int energyPoints);
+		void	setAttackDamage(int attackDamage);
+		void	setName(str name);
+
+		virtual void	attack(const str &target); // enforces polymorphism
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+	
+	protected:                       // using private here still works if setters and getters functions are used...
+		str	name;
+		int	hitPoints;
+		int	energyPoints;
+		int	attackDamage;
+};
+
+#endif
