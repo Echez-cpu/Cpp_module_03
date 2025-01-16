5f43317dd3ebf92ac9ecaead7497a2ae91ad58fc
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-01-16 20:03:26 +0100

    Create FragTrap.hpp

---
+++ ex03/FragTrap.hpp
@@ -0,0 +1,19 @@
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap : virtual public ClapTrap {
+	public:
+		FragTrap(str name);
+		FragTrap(FragTrap const &source);
+		~FragTrap();
+
+		FragTrap& operator=(FragTrap const &original_copy);
+		
+	void	highFivesGuys(void);
+
+  private:
+};
+
+#endif
